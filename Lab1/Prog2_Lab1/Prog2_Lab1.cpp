c60a236f347428a77f50c47321a5a6dc8ea80cf6
Alexander-Steblyanko <54948664+Alexander-Steblyanko@users.noreply.github.com>
2020-02-18 23:53:06 +0300

    First lab

---
+++ Lab1/Prog2_Lab1/Prog2_Lab1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+void plus_one(int &a);
+
+bool lesser_than(int a, int b);
+
+int main()
+{
+    
+    setlocale(LC_ALL, "Russian");
+    int firstnum, secnum, thirdnum;
+    cout << "Введите число для операції +1: ";
+    cin >> firstnum;
+    plus_one(firstnum);
+    cout << firstnum;
+    
+    cout << "Введите перше число для операції <: ";
+    cin >> secnum;
+    cout << "Введите друге число для операції <: ";
+    cin >> thirdnum;
+    cout << "Відношення <: " << lesser_than(secnum, thirdnum);
+}
+
+void plus_one(int &a) {
+    int sign = a >> 15;
+    for (int i = 0; i <= 15; i++) {
+        int bit = a & (1 << i);
+        a ^= (1 << i);
+        // cout << a << "\n";
+        if ((sign == -1 && bit == (1 << i)) || (sign == 0 && bit == 0)) { return; }
+    }
+}
+
+bool lesser_than(int a, int b) {
+    int bitA = a >> 15; 
+    int bitB = b >> 15;
+    if (bitA != bitB)
+        return bitA;
+    else
+        for (int i = 14; i >= 0; i--) {
+            bitA = (a >> i) & 1;
+            bitB = (b >> i) & 1;
+            if (bitA != bitB)
+                return bitB;
+        }
+    return 0;
+};
---
+++ Lab1/Prog2_Lab1_Cs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prog2_Lab1_Cs
+{
+    class Program
+    {
+        static void plus_one(ref int a)
+        {
+            int sign = a >> 31;
+            for (int i = 0; i <= 31; i++)
+            {
+                int bit = a & (1 << i);
+                a ^= (1 << i);
+                // Console.WriteLine(a);
+                if ((sign == -1 && bit == (1 << i)) || (sign == 0 && bit == 0)) { return; }
+            }
+        }
+        static bool lesser_than(int a, int b)
+        {
+            int bitA = a >> 31;
+            int bitB = b >> 31;
+            if (bitA != bitB)
+                return Convert.ToBoolean(bitA);
+            else
+                for (int i = 14; i >= 0; i--)
+                {
+                    bitA = (a >> i) & 1;
+                    bitB = (b >> i) & 1;
+                    if (bitA != bitB)
+                        return Convert.ToBoolean(bitB);
+                }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int firstnum, secnum, thirdnum;
+            Console.Write("Введите число для операції +1: ");
+            firstnum = Convert.ToInt32(Console.ReadLine());
+            plus_one(ref firstnum);
+            Console.WriteLine(firstnum);
+            
+            Console.Write("Введите перше число для операції <: ");
+            secnum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите друге число для операції <: ");
+            thirdnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Відношення <: " + lesser_than(secnum,
+                                                             thirdnum));
+        }
+    }
+}
