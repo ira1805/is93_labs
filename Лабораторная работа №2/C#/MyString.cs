ebc899849afbd160f9405aa94ae5846aa03fbfb4
SashaSamilenko <samilenko89@gmail.com>
2020-03-07 09:46:23 +0200

    New Laba_2

---
+++ Лабораторная работа №2/C#/MyString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibrary
+{
+    public class MyString
+    {
+		public char[] arrgs;
+		public int count = 0;
+		public MyString(char[] line = null)
+		{
+			arrgs = line;
+		}
+		public int СharCount(MyString arr, char c)
+		{
+			int k = 0;
+			for (int j = 0; j < arr.count; j++)
+			{
+				if (arr.arrgs[j] == c)
+				{
+					k += 1;
+				}
+			}
+			return k;
+		}
+		public void outputString()
+		{
+			for (int i = 0; i < arrgs.Length; i++)
+			{
+				if (arrgs[i] == default(char))
+					break;
+				Console.Write(arrgs[i]);
+			}
+		}
+		public void GetStr(char[] arr)
+		{
+			arrgs = arr;
+			for (int i = 0; i < 1000; i++)
+			{
+				if (arr[i] == default(char))
+					break;
+				count++;
+			}
+		}
+		~MyString()
+		{
+		}
+	}
+}
---
+++ Лабораторная работа №2/C#/MyText.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringLibrary;
+
+namespace TextLibrary
+{
+	public class MyText
+    {
+		public int count;
+		public MyString[] arrgs = new MyString[100];
+		public void AddStr_2(MyString text)
+		{
+			arrgs[count++] = text;
+		}
+		public void DeleteStr_2(int a)
+		{
+			for (int i = a - 1; i < count; i++)
+			{
+				arrgs[i] = arrgs[i + 1];
+			}
+			count--;
+		}
+		public void DeleteText_2()
+		{
+			for (int i = 0; i < count; i++)
+			{
+				arrgs[i] = new MyString();
+			}
+			count = 0;
+		}
+		public void MinStr_2(char[] a)
+		{
+			int min = arrgs[0].count;
+			int i_m = 0;
+			for (int i = 0; i < count; i++)
+			{
+				if (arrgs[i].count < min)
+				{
+					min = arrgs[i].count;
+					i_m = i;
+				}
+			}
+			int i_2 = 0;
+			for (int j = 0; j < arrgs[i_m].count; j++)
+			{
+				a[i_2] = arrgs[i_m].arrgs[j];
+				i_2++;
+			}
+		}
+		public void FirstChars_2(char[] a)
+		{
+			int j = 0;
+			for (int i = 0; i < count; i++)
+			{
+				a[j] = arrgs[i].arrgs[0];
+				j++;
+			}
+		}
+		public int CharCountInTextAndStr_2(char c)
+		{
+			int s = 0;
+			for (int i = 0; i < count; i++)
+			{
+				int Counte = arrgs[i].СharCount(arrgs[i], c);
+				s += Counte;
+			}
+			return s;
+		}
+	}
+}
