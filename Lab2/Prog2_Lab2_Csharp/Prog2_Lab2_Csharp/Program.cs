205873b2ec03a61d83fe9673d68e9e1342cdc751
Alexander-Steblyanko <54948664+Alexander-Steblyanko@users.noreply.github.com>
2020-03-11 09:23:02 +0300

    uncommented

--- Lab2/Prog2_Lab2_Csharp/Prog2_Lab2_Csharp/Program.cs
+++ Lab2/Prog2_Lab2_Csharp/Prog2_Lab2_Csharp/Program.cs
@@ -8,13 +8,13 @@ namespace Prog2_Lab2_Csharp
         static void Main(string[] args)
         {
             TextSharp textA = new TextSharp(new string[3] { "Hello world", "This is a text", "Hello world" });
-//            Console.WriteLine("Number of chracters: " + textA.CountAll().ToString());
-//          Console.WriteLine("Number of 'Hello world's: " + textA.FindCount("Hello world").ToString());
-//            textA.AddLine(1, "A fresh new line");
-//           textA.RemoveLine(2);
-//           textA.FindAndReplaceAll('e', 'x');
-//           Console.WriteLine(textA);
-//           textA.CleanAll();
+            Console.WriteLine("Number of chracters: " + textA.CountAll().ToString());
+            Console.WriteLine("Number of 'Hello world's: " + textA.FindCount("Hello world").ToString());
+            textA.AddLine(1, "A fresh new line");
+            textA.RemoveLine(2);
+            textA.FindAndReplaceAll('e', 'x');
+            Console.WriteLine(textA);
+            textA.CleanAll();
         }
     }
 }
