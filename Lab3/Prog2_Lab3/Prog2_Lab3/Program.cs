df83d0b414ccedb0d5c1d3bfe744cd2747ba5b37
Alexander-Steblyanko <54948664+Alexander-Steblyanko@users.noreply.github.com>
2020-03-17 18:17:39 +0300

    added Lab 3

---
+++ Lab3/Prog2_Lab3/Prog2_Lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Prog2_Lab3_Lib;
+
+namespace Prog2_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SymbolMatrix sym = new SymbolMatrix(new string[2] { "Hello world", "This is a 2d array" }); // Демонстрація конструктора
+            WriteOut(sym);
+            Console.WriteLine();
+
+            Console.Write("Другий символ кожного рядка: ");
+            Console.WriteLine(sym[1]); // Демонстрація get індексатора
+            sym[1] = new char[2] { 'i', 'i' }; // Демонстрація set індексатора
+            Console.Write("Другий символ кожного рядка пiсля змiни: "); 
+            Console.WriteLine(sym[1]);
+            Console.Write("Двадцятий символ кожного рядка: ");
+            Console.WriteLine(sym[19]); // Демонстрація get індексатора для значень індекса більших розміра рядка
+            Console.WriteLine();
+
+            WriteOut(sym);
+            Console.WriteLine();
+
+            Console.WriteLine("Кiлькiсть цифр в усiх рядках: " + sym.NumofNums); // Демонстрація властивості читання кількості цифр
+        }
+
+        // Метод для виведення масиву на екран. Поза класу бо використовує консоль.
+        static void WriteOut(SymbolMatrix input)
+        {
+            for (int i = 0; i < input.GetWhole().Length; i++)
+            {
+                Console.WriteLine(input.GetWhole()[i]);
+            }
+            return;
+        }
+    }
+}
---
+++ Lab3/Prog2_Lab3/Prog2_Lab3_Lib/SymbolMatrix.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Prog2_Lab3_Lib
+{
+    public class SymbolMatrix
+    {
+        // Закритий двовимірний масив елементів символьного типу
+        private char[][] mat;
+        // Закритий елемент, що містить кількість цифр у масиві 
+        private int numofNums;
+
+        // Конструктор класа
+        public SymbolMatrix(string[] input)
+        {
+            mat = new char[input.Length][];
+            for (int i = 0; i < input.Length; i++)
+            {
+                mat[i] = new char[input[i].Length];
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    mat[i][j] = input[i][j];
+                }
+            }
+
+        }
+
+        // Одновимірний індексатор
+        // get повертає рядок зі значеннями заданого стовпця масиву 
+        // set записує значення з введеного рядка в стовпець масиву 
+        public char[] this[int index]
+        {
+            get
+            {
+                char[] column = new char[this.mat.Length];
+                for (int i = 0; i < this.mat.Length; i++)
+                {
+                    if (index < this.mat[i].Length)
+                    {
+                        column[i] = this.mat[i][index];
+                    }
+                    // Якщо рядок коротший індекса, додає нульовий символ
+                    else
+                    {
+                        column[i] = '\0';
+                    }
+                }
+                return column;
+            }
+            set
+            {
+                for (int i = 0; i < mat.Length; i++)
+                {
+                    mat[i][index] = value[i];
+                }
+            }
+        }
+
+        // Властивість для читання кількості цифр у масиві
+        // Так як кількість цифр у масиві залежить від масиву, розраховує кількість, зберігає її в елементі numofNums, котрий повертає
+        public int NumofNums
+        {
+            get
+            {
+                numofNums = 0;
+                for (int i = 0; i < mat.Length; i++)
+                {
+                    for (int j = 0; j < mat[i].Length; j++)
+                    {
+                        if (Char.IsDigit(mat[i][j]))
+                        {
+                            numofNums++;
+                        }
+                    }
+                }
+                return numofNums;
+            }
+        }
+
+        // Повертає весь массив, для простоти виводу
+        public char[][] GetWhole()
+        {
+            return mat;
+        }
+    }
+}
