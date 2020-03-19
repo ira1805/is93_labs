f67846a8ef042527a0cf48c941149ef05b049be6
dimas7001 <54948234+dimas7001@users.noreply.github.com>
2020-03-19 09:17:20 +0200

    laba3 (updated + comments)

--- laba3/Program.cs
+++ laba3/Program.cs
@@ -5,38 +5,51 @@ namespace laba3
 {
     public class MyArray
     {
-        private List<int> m_data;
-        public MyArray()
+        private List<int> m_data;       //створення змінних, що зберігають дані класса
+        private int m_size = 0;
+        public MyArray()                //конструктор
         {
-            m_data = new List<int>();
+            m_data = new List<int>();   //присвоєння значення пустого списку змінній
         }
 
-        public void Add(int input)
+        public void Add(int input)  //метод додавання
         {
-            m_data.Add(input);
+            m_data.Add(input);      //додавання елементу в масив
+            m_size += 1;            //інкрементація змінної розмірності
         }
 
-        public int Size()
-        {
-            return m_data.Count;
-        }
-
-        public int this[int index]
+        public int Size             //властивість виведення розміру
         {
             get
             {
-                return m_data[index];
-            }
-            set
-            {
-                m_data[index] = value;
+                return m_size;      //виведення змінної, що відповідає за розмір
             }
         }
 
-        public override string ToString()
+        public int this[int index]  //індексатор
+        {
+            get
+            {
+                if (index >= 0 && index < m_data.Count) //умова не порушення границі масиву
+                {
+                    return m_data[index];               //повернення елемента масиву
+                }
+                else                                    //випадок порушення границі
+                {
+                    return (1234567890);                //повернення помилки
+                }
+                
+            }
+            set
+            {
+                m_data[index] = value;                  //зміна елемента масиву за індексом
+            }
+        }
+
+        public override string ToString()               //метод для виведення вмісту
         {
             string output = "";
-            foreach (var x in m_data)
+            foreach (var x in m_data)                   //цикл виведення вмісту
                 output += x.ToString() + "  ";
             return output;
         }
@@ -46,13 +59,15 @@ namespace laba3
     {
         static void Main(string[] args)
         {
-            MyArray arr = new MyArray();
-            arr.Add(1);
+            MyArray arr = new MyArray();                //створення класу
+            arr.Add(1);                                 //заповнення класу
             arr.Add(2);
             arr.Add(3);
             arr.Add(4);
             arr.Add(5);
             Console.WriteLine("Baranov Dmytro IS-93");
+            Console.WriteLine();
+            Console.WriteLine("message 1234567890 means error");
             Console.WriteLine();
             Console.WriteLine("array");
             Console.WriteLine(arr);
@@ -60,8 +75,11 @@ namespace laba3
             Console.WriteLine("fourth element");
             Console.WriteLine(arr[3]);
             Console.WriteLine();
+            Console.WriteLine("sixth element");
+            Console.WriteLine(arr[5]);
+            Console.WriteLine();
             Console.WriteLine("size of array");
-            Console.WriteLine(arr.Size());
+            Console.WriteLine(arr.Size);
 
             Console.ReadKey();
         }
