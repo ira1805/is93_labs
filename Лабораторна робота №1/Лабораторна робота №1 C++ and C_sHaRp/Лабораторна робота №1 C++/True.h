d8e32f16031ad830905456552ce2bc0f05c01e37
SashaSamilenko <samilenko89@gmail.com>
2020-04-06 00:15:37 +0300

    Add lab of 5

--- Лабораторна робота №1/Лабораторна робота №1 C++ and C_sHaRp/Лабораторна робота №1 C++/True.h
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-namespace New
-{
-	void Counter(int &);//Це функція для зменшення числа на 1;
-	int Summa_Bit(int, int);//Це функція для суми двох чисел.
-	void Output(int);//Output the value в двійковій System!;
-}
