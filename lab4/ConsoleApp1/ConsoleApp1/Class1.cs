0a65bf8ade96c77550d4fb8a6c15b95ff2fb32a5
ootro <ootro@users.noreply.github.com>
2020-04-15 11:25:15 +0300

    Laboratory work #4 and #5
    Laboratory work #4 and #5

---
+++ lab4/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MyClass
+    {
+        private char[] str;
+
+        public MyClass() //конструктор за замовчуванням
+        {
+
+        }
+        public MyClass(char[] str) //конструктор з параметром
+        {
+            this.str = str;
+        }
+
+        public MyClass(MyClass other) //конструктор з копіюванням
+        {
+            this.str = other.str;
+        }
+
+        public char[] getLine() //метод виведення рядку
+        {
+            return str;
+        }
+
+        public int getLength() //метод знаходження довжини рядку
+        {
+            return str.Length;
+        }
+
+        public int getSize(MyClass obj, char symbol) //знаходження довижини кожного рядку
+        {
+            int Size = obj.str.Length;
+            for (int i = 0; i < obj.str.Length; i++)
+                if (obj.str[i] == symbol)
+                    Size = obj.str.Length - 1;
+            return Size;
+        }
+        public static MyClass operator -(MyClass obj, char symbol) //оператор видалення символу з рядка
+        {
+            MyClass a = new MyClass();
+            int size = obj.getSize(obj, symbol);
+            char[] tmp = new char[size];
+            int k = 0;
+            for (int i = 0; i < obj.str.Length; i++)
+            {
+                if (obj.str[i] != symbol)
+                    tmp[i - k] = obj.str[i];
+                else
+                    k++;
+            }
+            a.str = tmp;
+            return a;
+        }
+
+        public static MyClass operator +(MyClass obj1, MyClass obj2) //оператор "складення" об'єктів
+        {
+            MyClass c = new MyClass();
+            string txt1 = new string(obj1.str);
+            string txt2 = new string(obj2.str);
+            string result = Convert.ToString(Convert.ToInt32(txt1) + Convert.ToInt32(txt2));
+            char[] arr;
+            arr = result.ToCharArray();
+            c.str = arr;
+            return c;
+        }
+    }
+}
+
---
+++ lab4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine(); //введення рядків 
+            string str2 = Console.ReadLine();
+            char[] arr1; //оголошуємо масиви символів
+            char[] arr2;
+            arr1 = str1.ToCharArray(); //ініціалізуємо їх
+            arr2 = str2.ToCharArray();
+
+            MyClass a = new MyClass(arr1); //об'єкт C2
+            MyClass b = new MyClass(arr2); //об'єкт С3
+            MyClass c = new MyClass(a + b); //об'єкт С1
+
+            Console.WriteLine("C2: ");
+            Console.WriteLine(a.getLine());
+            Console.WriteLine("C2 length: ");
+            Console.WriteLine(a.getLength());
+            a = a - '5';
+            Console.WriteLine("C2 without 5: ");
+            Console.WriteLine(a.getLine());
+            Console.WriteLine(c.getLine());
+        }
+    }
+}
