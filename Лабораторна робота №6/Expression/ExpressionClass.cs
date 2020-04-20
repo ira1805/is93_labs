81252d40bb2f41d4b3caa063d143ab9b7ae83ae0
SashaSamilenko <samilenko89@gmail.com>
2020-04-20 17:23:36 +0300

    Add Laba 6 on C#

---
+++ Лабораторна робота №6/Expression/ExpressionClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+    public class ExpressionClass
+    {
+        private double a, b, c, d;//Our values
+        public ExpressionClass(double a,double b,double c,double d)//Constructor with parametrs
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+        public double Value()//Method for output of value our Expression
+        {
+            if (d > 41)//If d>41 i show massage
+            {
+                throw new DivideByZeroException("Expression have a zero the root!");//Output message with exception
+            }
+            else
+            {
+                if (Math.Sqrt(41 - d) - b * a + c == 0)//If denominator = 0 i show massage with exception
+                {
+                    throw new DivideByZeroException("Expression have a zero the denominator!");//I show message with exception
+                }
+                else
+                {
+                    return(((a * b / 4.0) - 1) / (Math.Sqrt(41 - d) - b * a + c));//Else, i return value of expression
+                }
+            }
+        }
+    }
+}
---
+++ Лабораторна робота №6/Лабораторна робота №6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expression;
+
+namespace Лабораторна_робота__6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionClass[] obj = new ExpressionClass[5];// Make arrg out objects
+            obj[0] = new ExpressionClass(1, 2, 3, 41);
+            obj[1] = new ExpressionClass(5, 2, 3, 2);
+            obj[2] = new ExpressionClass(4, 2, 5, 10);
+            obj[3] = new ExpressionClass(1, 41, 12, 20);
+            obj[4] = new ExpressionClass(4, 13, 3, 15);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Value {i + 1} = {Math.Round(obj[i].Value(),5)}");//Output value each object
+            }
+            Console.ReadKey();
+        }
+    }
+}
