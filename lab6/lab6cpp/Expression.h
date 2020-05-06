080e73324b4c29fc3951db1a9bbdf499b247fda8
mahlaiv <49984303+mahlaiv@users.noreply.github.com>
2020-05-06 22:30:15 +0300

    Create Expression.h

---
+++ lab6/lab6cpp/Expression.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+class Expression
+{
+public:
+	double a;
+  double b;
+	double c;
+	double d;
+
+	Expression(double a, double b,  double c, double d);
+
+	double Calculate();
+
+	Expression* operator +(int num);
+
+	Expression* operator +(Expression exp2);
+
+	Expression* operator -(int num);
+
+	Expression* operator -(Expression exp2);
+
+}; 
