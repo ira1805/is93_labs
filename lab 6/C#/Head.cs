41c6dac4bf6a32bd1d522b406e83f3aa9fed9363
Torden <55050547+Rogue6X@users.noreply.github.com>
2020-05-24 00:14:07 +0300

    Adding lab 6 after I forgot to add it

---
+++ lab 6/C#/Head.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ooplab6
+{
+    class Vars
+    {
+
+        public double A { set; get; }
+        public double B { set; get; }
+        public double C { set; get; }
+        public double D { set; get; }
+
+        public Vars(double a, double b, double c, double d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+        public static void log(string error)
+        {
+            File.WriteAllText(@"log.txt", error);
+            
+        }
+
+        public double Calc()
+        {
+            if (B == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            if (24 + D - C < 0)
+            {
+                throw new InvalidDataException();
+            }
+            return (1 + A - B / 2) / (Math.Sqrt(24 + D - C) + A / B);
+        }
+
+
+    }
+}
