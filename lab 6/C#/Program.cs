6328229ab9b00e02970af95eb4655e42a179d876
Torden <55050547+Rogue6X@users.noreply.github.com>
2020-05-24 00:31:30 +0300

    Add files via upload

---
+++ lab 6/C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ooplab6
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            try
+            {
+                //Vars vars = new Vars(5, 0, 6, 1);
+                Vars vars = new Vars(2, 5, 23, -3);
+                Console.WriteLine(vars.Calc());
+            }
+
+            catch (DivideByZeroException err)
+            {
+                Vars.log($"Exception: {err.GetType()}, time: {DateTime.Now}");
+            }
+            catch (InvalidDataException err)
+            {
+                Vars.log($"Exception: {err.GetType()}, time: {DateTime.Now}");
+            }
+
+            finally
+            {
+                Console.WriteLine("End");
+            }
+        }
+    }
+}
