3c9cfa9a51ca9427a7717b353e24cd7016a3df68
Malanchyk <56121409+Malanchyk@users.noreply.github.com>
2020-06-04 19:59:51 +0300

    Add files via upload

---
+++ Afisha/ITicket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Afisha
+{
+    public interface ITicket
+    {
+        public void Reset();
+        public void Book();
+        public void Buy();
+        
+        public TicketPlace TicketPlace
+        {
+            get;
+        }
+
+        public TicketStatus TicketStatus
+        {
+            get;
+        }
+
+        public string PerfomanceName
+        {
+            get;
+        }
+    }
+}
